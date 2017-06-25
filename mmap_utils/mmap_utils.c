7e4bd3113332f20c8850e30e363ee1a7752ebb1e
John Haynes <john.andrew.haynes@gmail.com>
2017-06-25 17:56:01 +0200

    mmap utils

---
+++ mmap_utils/mmap_utils.c
@@ -0,0 +1,27 @@
+#include "mmap_utils.h"
+
+mapped_file_t map_file_open(const char *file_name) {
+
+    struct stat sbuf;
+    int fd = open(file_name,O_RDONLY);
+    fstat(fd, &sbuf);
+    size_t szBytes = sbuf.st_size;
+    int64_t pagesize = sysconf(_SC_PAGE_SIZE);
+    int64_t sz_aligned = szBytes + pagesize - (szBytes % pagesize);
+    uint8_t *mmapPtr = mmap(0, sz_aligned, PROT_READ, MAP_SHARED, fd, 0);
+
+    mapped_file_t mf = {
+        .sz = szBytes,
+        .dat = mmapPtr,
+        .sz_aligned = sz_aligned,
+        .file_name = file_name,
+        .fd = fd
+    };
+
+    return mf;
+}
+
+void map_file_close(const mapped_file_t mf) {
+  munmap(mf.dat, mf.sz_aligned);
+  close(mf.fd);
+}
---
+++ mmap_utils/mmap_utils.h
@@ -0,0 +1,22 @@
+#ifndef MMAP_UTILS_H
+#define MMAP_UTILS_H
+
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+typedef struct {
+    const size_t sz;
+    uint8_t *dat;
+    const size_t sz_aligned;
+    const char *file_name;
+    const int fd;
+} mapped_file_t;
+
+mapped_file_t map_file_open(const char *file_name);
+
+void map_file_close(const mapped_file_t mf);
+
+#endif
